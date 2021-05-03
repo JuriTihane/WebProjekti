eba43f89236847a087ca0675f8b68bb768721400
juritihane <juri.tihane@gmail.com>
2021-05-03 13:52:26 +0300

    Commit fix

--- app/src/App.vue
+++ app/src/App.vue
@@ -23,13 +23,13 @@ export default {
 </script>
 
 <style>
-/*#app {*/
-/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
-/*  -webkit-font-smoothing: antialiased;*/
-/*  -moz-osx-font-smoothing: grayscale;*/
-/*  text-align: center;*/
-/*  color: #2c3e50;*/
-/*}*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
 body {
   background-color: white;
